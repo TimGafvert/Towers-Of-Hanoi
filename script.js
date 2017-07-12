f7e0d415c380799a462e52aa3755f83fcf01ced7
Admin <Tgafvert@gmail.com>
2017-07-12 11:54:23 -0400

    Simplified Logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* global $ */
 $(document).ready(function () {
   var ringSmall = $('#ringSmall')
   var ringMedium = $('#ringMedium')
@@ -5,22 +6,46 @@ $(document).ready(function () {
   var pole1 = $('#pole1')
   var pole2 = $('#pole2')
   var pole3 = $('#pole3')
+  var selectedRing
 
-  ringSmall.on('click', selectRing.bind(null, 1))
-  ringMedium.on('click', selectRing.bind(null, 2))
-  ringLarge.on('click', selectRing.bind(null, 3))
+  pole1.on('click', selectPole.bind(null, 1))
+  pole2.on('click', selectPole.bind(null, 2))
+  pole3.on('click', selectPole.bind(null, 3))
 
-  function selectRing (size) {
-    switch (size) {
+  function selectPole (num) {
+    switch (num) {
       case 1:
-        $('#ringSmall').detach()
+        selectedRing = $(' #pole1 div:last-child')
+        selectedRing.detach()
         break
       case 2:
-        $('#ringMedium').detach()
-        break
+      selectedRing = $(' #pole1 div:last-child')
+      selectedRing.detach()
       case 3:
-        $('#ringLarge').detach()
+      selectedRing = $(' #pole1 div:last-child')
+      selectedRing.detach()
         break
     }
   }
 })
+
+// ringSmall.on('click', selectRing.bind(null, 1))
+// ringMedium.on('click', selectRing.bind(null, 2))
+// ringLarge.on('click', selectRing.bind(null, 3))
+
+// function selectRing (size) {
+//   switch (size) {
+//     case 1:
+//       $('#ringSmall').detach()
+//       pole2.append(ringSmall)
+//       break
+//     case 2:
+//       $('#ringMedium').detach()
+//       pole2.append(ringMedium)
+//       break
+//     case 3:
+//       $('#ringLarge').detach()
+//       pole2.append(ringLarge)
+//       break
+//   }
+// }
