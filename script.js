b5826f08ec7e45fe5a5445c91f100bbe8febcc2a
Admin <Tgafvert@gmail.com>
2017-07-13 10:06:57 -0400

    everything works, bronze level. Time to split into Model and View

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
   var selectedRing
   var comparedRing
   var ringIsSelected = false
+  console.log(pole2.length)
 
   // When a pole is clicked, select pole to get or place a ring
   pole1Display.on('click', function () {
@@ -43,39 +44,45 @@ $(document).ready(function () {
             pole1Display.children().last().detach()
           }
         } else {
-          ringIsSelected = false
-          pole1.push(hand)
-          pole1Display.append(handDisplay)
+          if ((pole1.length === 0) || (pole1[pole1.length - 1] < hand)) {
+            ringIsSelected = false
+            pole1.push(hand)
+            pole1Display.append(handDisplay)
+          }
         }
         break
       case 2:
-      if (!ringIsSelected) {
-        if (pole2.length > 0) {
-          ringIsSelected = true
-          hand = pole2.pop()
-          handDisplay = pole2Display.children().last()
-          pole2Display.children().last().detach()
+        if (!ringIsSelected) {
+          if (pole2.length > 0) {
+            ringIsSelected = true
+            hand = pole2.pop()
+            handDisplay = pole2Display.children().last()
+            pole2Display.children().last().detach()
+          }
+        } else {
+          if ((pole2.length === 0) || (pole2[pole2.length - 1] < hand)) {
+            ringIsSelected = false
+            pole2.push(hand)
+            pole2Display.append(handDisplay)
+          }
         }
-      } else {
-        ringIsSelected = false
-        pole2.push(hand)
-        pole2Display.append(handDisplay)
-      }
-      break
+        break
       case 3:
-      if (!ringIsSelected) {
-        if (pole3.length > 0) {
-          ringIsSelected = true
-          hand = pole3.pop()
-          handDisplay = pole3Display.children().last()
-          pole3Display.children().last().detach()
+        if (!ringIsSelected) {
+          if (pole3.length > 0) {
+            ringIsSelected = true
+            hand = pole3.pop()
+            handDisplay = pole3Display.children().last()
+            pole3Display.children().last().detach()
+          }
+        } else {
+          if ((pole3.length === 0) || (pole3[pole3.length - 1] < hand)) {
+            ringIsSelected = false
+            pole3.push(hand)
+            pole3Display.append(handDisplay)
+          }
         }
-      } else {
-        ringIsSelected = false
-        pole3.push(hand)
-        pole3Display.append(handDisplay)
-      }
-      break
+        break
     }
   }
 })
